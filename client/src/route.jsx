9ec237cb15f5c133d4fdb349aa5b96d8851f6476
Badabala <116370080+Badabala@users.noreply.github.com>
2024-04-13 17:20:29 +0700

    Commit message here

--- client/src/route.jsx
+++ client/src/route.jsx
@@ -1,6 +1,10 @@
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import Login from "./pages/login";
 import HomePage from "./pages/homePage";
+import JobDetail from "./pages/jobDetail";
+import Company from "./pages/createACompany";
+// import Layout from "./layout";
+
 const publicPath = [
   {
     path: "/login",
@@ -27,6 +31,33 @@ const publicPath = [
       icon: <PermIdentityIcon fontSize="large" />,
     },
   },
+
+
+  {
+    path: "/jobdetail",
+    content: [
+      {
+        component: <JobDetail />,
+      },
+    ],
+    propsLayout: {
+      title: "Job Detail",
+      icon: <PermIdentityIcon fontSize="large" />,
+    },
+  },
+  {
+    path: "/company",
+    content: [
+      {
+        component: <Company/>,
+      },
+    ],
+    propsLayout: {
+      title: "Create a Company",
+      icon: <PermIdentityIcon fontSize="large" />,
+    },
+  },
+
 ];
 
 export const route = {
